68a312fab1d464dc3d19a80b0d67eadfb6ffdb75
Guybrush3791 <giovanni@wows.dev>
2025-09-16 11:09:39 +0200

    update

---
+++ aider-memory/constraints.md
@@ -0,0 +1,13 @@
+# Aider Constraints
+
+This is last version of React in JavaScript, 
+project is based on Vite and `StrictMode` is activated
+
+## Style
+
+Rely only on React component and class, avoid custom style for simple component,
+never use style attribute in HTML
+
+## Focus
+
+Simplicity and implementation speed are paramount
--- src/App.jsx
+++ src/App.jsx
@@ -3,81 +3,10 @@ import './App.css'
 
 function App() {
   
-
   return (
-    <main className="app">
-      <h1 className="title">*** Complaining form! ***</h1>
-      <form className="complaint-form" action="#" method="post">
-        <div className="form-grid two-columns">
-          <div className="left">
-            <div className="form-field">
-              <label htmlFor="fullName">Full name</label>
-              <input type="text" id="fullName" name="fullName" />
-            </div>
-
-            <div className="form-field">
-              <label htmlFor="address">Address</label>
-              <input type="text" id="address" name="address" />
-            </div>
-
-            <div className="form-field">
-              <label htmlFor="phone">Phone Number</label>
-              <input type="tel" id="phone" name="phone" />
-            </div>
-
-            <div className="form-field">
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" />
-            </div>
-          </div>
-
-          <div className="right">
-            <div className="form-field">
-              <label htmlFor="complaint">Write your complaint</label>
-              <textarea
-                id="complaint"
-                name="complaint"
-                rows={10}
-                placeholder="You can complain here"
-              ></textarea>
-            </div>
-
-            <fieldset className="form-field">
-              <legend>How do you want to be contacted?</legend>
-              <div className="radio-group">
-                <label>
-                  <input type="radio" name="contactMethod" value="phone" defaultChecked />
-                  Phone
-                </label>
-                <label>
-                  <input type="radio" name="contactMethod" value="email" />
-                  Email
-                </label>
-                <label>
-                  <input type="radio" name="contactMethod" value="slowmail" />
-                  Slow Mail
-                </label>
-                <label>
-                  <input type="radio" name="contactMethod" value="none" />
-                  No contact!
-                </label>
-              </div>
-            </fieldset>
-
-            <div className="form-field">
-              <label className="checkbox">
-                <input type="checkbox" name="consent" />
-                I agree you take my data, and do whatever
-              </label>
-            </div>
-
-            <div className="actions">
-              <button type="submit">Submit!</button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </main>
+    <>
+      
+    </>
   )
 }
 
